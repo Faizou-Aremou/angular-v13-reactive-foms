3baa82be6edc70e50b7d7ad32ffa102f2ee5e859
Faizou <faizouaremou@gmail.com>
2023-09-07 23:53:29 +0200

    validator function

--- libs/section2/src/lib/lesson3/hero-validation/hero-validation.component.ts
+++ libs/section2/src/lib/lesson3/hero-validation/hero-validation.component.ts
@@ -3,7 +3,7 @@ import {
   FormControl,
   FormGroup,
   ValidatorFn,
-  Validators
+  Validators,
 } from '@angular/forms';
 
 const stats = ['attack', 'defense', 'speed', 'health'];
@@ -15,12 +15,20 @@ const MAX_TOTAL_STATS = 60;
 const singleStatValidators = [
   Validators.required,
   Validators.min(MIN_SINGLE_STAT),
-  Validators.max(MAX_SINGLE_STAT)
+  Validators.max(MAX_SINGLE_STAT),
 ];
 
 const heroValidator: ValidatorFn = (control: FormGroup) => {
-  // define the validator here!!
-  return null;
+  const stats = control.value;
+  console.log('stats', stats);
+  const statsValuesSomme = (Object.values(stats) as number[]).reduce(
+    (somme, stat) => {
+      return (somme + stat) as number;
+    },
+    0 as number
+  );
+  console.log('statsValuesSomme', statsValuesSomme);
+  return statsValuesSomme > 60 ? { maxTotal: statsValuesSomme } : null;
 };
 
 const createSingleStatControl = () => new FormControl(0, singleStatValidators);
@@ -28,6 +36,26 @@ const createSingleStatControl = () => new FormControl(0, singleStatValidators);
 @Component({
   selector: 'forms-course-hero-validation-2',
   templateUrl: './hero-validation.component.html',
-  styleUrls: ['./hero-validation.component.css']
+  styleUrls: ['./hero-validation.component.css'],
 })
-export class HeroValidationComponent {}
+export class HeroValidationComponent {
+  formGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    stats: new FormGroup(
+      {
+        attack: createSingleStatControl(),
+        defense: createSingleStatControl(),
+        speed: createSingleStatControl(),
+        health: createSingleStatControl(),
+      },
+      heroValidator
+    ),
+  });
+  stats = stats;
+  get statsGroup() {
+    return this.formGroup.get('stats') as FormGroup;
+  }
+  get name() {
+    return this.formGroup.get('name') as FormControl;
+  }
+}
