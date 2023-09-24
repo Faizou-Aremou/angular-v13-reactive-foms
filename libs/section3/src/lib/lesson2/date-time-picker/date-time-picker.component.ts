0cdbed1661571bb0f12f76ac1c8554c63c8d03f3
Faizou <faizouaremou@gmail.com>
2023-09-24 20:30:07 +0200

    work with formGroup

--- libs/section3/src/lib/lesson2/date-time-picker/date-time-picker.component.ts
+++ libs/section3/src/lib/lesson2/date-time-picker/date-time-picker.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy } from '@angular/core';
 import {
   ControlValueAccessor,
+  FormControl,
   FormGroup,
-  NG_VALUE_ACCESSOR
+  NG_VALUE_ACCESSOR,
 } from '@angular/forms';
-import { Subject } from 'rxjs';
+import { formatISO, formatISO9075 } from 'date-fns';
+import { Subject, startWith, takeUntil } from 'rxjs';
 
 const padZero = (n: number): string => (n < 10 ? `0${n}` : `${n}`);
 const timeString = (date: Date) =>
@@ -24,34 +26,53 @@ const dateString = (date: Date) =>
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: DateTimePickerComponent,
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+  ],
 })
 export class DateTimePickerComponent
-  implements OnDestroy, ControlValueAccessor {
+  implements OnDestroy, ControlValueAccessor
+{
   formGroup: FormGroup;
   private _destroying$ = new Subject<void>();
   private _onTouched;
+  private _onChanged;
+  isDisabled = false;
 
   writeValue(date: Date) {
-    // create your implementation here!
+    if (!this.formGroup) {
+      this.formGroup = new FormGroup({
+        date: new FormControl(formatISO(date, { representation: 'date' })),
+        time: new FormControl(formatISO9075(date, { representation: 'time' })),
+      });
+    } else {
+      this.formGroup.patchValue({
+        date: formatISO(date, { representation: 'date' }),
+        time: formatISO9075(date, { representation: 'time' }),
+      });
+      this._onChanged(date);
+    }
   }
 
   registerOnChange(fn) {
-    // create your implementation here!
+    this._onChanged = fn;
+    this.formGroup.valueChanges
+      .pipe(takeUntil(this._destroying$), startWith(this.formGroup.value))
+      .subscribe(({date, time}) => {
+        this._onChanged(date && time ? new Date(`${date} ${time}`) : null);
+      });
   }
 
   registerOnTouched(fn) {
-    // create your implementation here!
+    this._onTouched = fn;
   }
 
   blur() {
-    // create your implementation here!
+    this._onTouched();
   }
 
   setDisabledState(isDisabled: boolean) {
-    // create your implementation here!
+    isDisabled ? this.formGroup.disable(): this.formGroup.enable()
   }
 
   ngOnDestroy() {
