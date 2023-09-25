4ed5f19c6735f64e7910e9ddf1bb804961f0095a
Faizou <faizouaremou@gmail.com>
2023-09-25 23:17:02 +0200

    reusable control for complex objets

--- libs/section3/src/lib/lesson3/hero-form/hero-form.component.ts
+++ libs/section3/src/lib/lesson3/hero-form/hero-form.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnDestroy } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { Subject } from 'rxjs';
+import {
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { Subject, startWith, takeUntil } from 'rxjs';
 
 interface Hero {
   name: string;
@@ -8,7 +13,7 @@ interface Hero {
     attack: number;
     defense: number;
     speed: number;
-    health;
+    health: number;
   };
 }
 
@@ -19,31 +24,54 @@ const stats = ['attack', 'defense', 'speed', 'health'];
   templateUrl: './hero-form.component.html',
   styleUrls: ['./hero-form.component.css'],
   providers: [
-    { provide: NG_VALUE_ACCESSOR, useExisting: HeroFormComponent, multi: true }
-  ]
+    { provide: NG_VALUE_ACCESSOR, useExisting: HeroFormComponent, multi: true },
+  ],
 })
 export class HeroFormComponent implements OnDestroy, ControlValueAccessor {
   private _destroying$ = new Subject<void>();
   stats = stats;
+  formGroup: FormGroup;
+  private _onChanged: any;
+  private _onTouched: any;
 
+  get statsFormGroup(){
+    return this.formGroup.get('stats');
+  }
   writeValue(hero: Hero) {
-    // add your implementation here!
+    if (!this.formGroup) {
+      this.formGroup = new FormGroup({
+        name: new FormControl(hero.name),
+        stats: new FormGroup({
+          attack: new FormControl(hero.stats.attack),
+          defense: new FormControl(hero.stats.defense),
+          speed: new FormControl(hero.stats.speed),
+          health: new FormControl(hero.stats.health),
+        }),
+      });
+    } else {
+      this.formGroup.setValue(hero);
+    }
   }
 
   registerOnChange(fn) {
-    // add your implementation here!
+    this._onChanged = fn;
+    this.formGroup.valueChanges
+      .pipe(takeUntil(this._destroying$), startWith(this.formGroup.value))
+      .subscribe((formValue) => {
+        this._onChanged(formValue);
+      });
   }
 
   registerOnTouched(fn) {
-    // add your implementation here!
+    this._onTouched = fn;
   }
 
   blur() {
-    // add your implementation here!
+    this._onTouched();
   }
 
   setDisabledState(isDisabled: boolean) {
-    // add your implementation here!
+    isDisabled ? this.formGroup.disable() : this.formGroup.enable();
   }
 
   ngOnDestroy() {
